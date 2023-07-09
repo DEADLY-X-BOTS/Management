81aa6d1b63473ae4f1457f58c1187f4f220137ec
#Lord Lucifer ❤️ <137722535+DEADLY-X-BOTS@users.noreply.github.com>
2023-07-09 22:32:07 +0530

    Update config.py

--- MukeshRobot/config.py
+++ MukeshRobot/config.py
@@ -7,7 +7,7 @@ class Config(object):
     API_ID = "" # integer value, dont use ""
     API_HASH = ""
     TOKEN = ""  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 6092112324 # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_ID = 5434400982 # If you dont know, run the bot and do /id in your private chat with it, also an integer
     
     SUPPORT_CHAT = "LOVE_VIB3S"  # Your own group for support, do not add the @
     START_IMG = ""
